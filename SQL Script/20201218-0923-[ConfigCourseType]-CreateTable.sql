a9722bffb08894af3586f58630a1ef245a94dd8e
HoangVoMinh <hoangvm.hiephoa@gmail.com>
2020-12-18 10:00:33 +0700

    add: CourseLayout: add, edit, delete, login, register

---
+++ SQL Script/20201218-0923-[ConfigCourseType]-CreateTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `configcoursetype` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Log_CreatedDate` datetime DEFAULT NULL,
+  `Log_CreatedBy` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `Log_UpdatedDate` datetime DEFAULT NULL,
+  `Log_UpdatedBy` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `IsDeleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,37 @@ var router = express.Router();
 var CourseController = require('../Controller/CourseController')
 
 /* GET home page. */
+//--- Index
 router.get('/', function(req,res,next){
   res.render('index',{title:'Trang chủ'})
 });
+//---Login
+router.get('/login',function(req,res,next){
+  res.render('login',{title:'Login',layout:'loginLayout'})
+})
+router.post('/login',function(req,res,next){
+  res.redirect('/')
+})
+//---Register
+router.get('/register',function(req,res,next){
+  res.render('register',{title:'register',layout:'loginLayout'})
+})
+router.post('/register',function(req,res,next){
+  res.redirect('/login')
+})
+//---Course
 router.get('/courses-mobile',function(req,res,next){
-  res.render('courseMobile',{title:"Khóa học Mobile"})
+  res.render('./course/courseMobile',{title:"Khóa học Mobile"})
 })
 router.get('/courses-website',function(req,res,next){
-  res.render('courseWebsite',{title:"Khóa học Website"})
+  res.render('./course/courseWebsite',{title:"Khóa học Website"})
+})
+//---Add Course
+router.get('/add-course',function(req,res,next){
+  res.render('./course/addCourse',{title:'Add new course'})
+})
+//---Edit Course
+router.get('/edit-course',function(req,res,next){
+  res.render('./course/editCourse',{title:'Edit course'})
 })
 module.exports = router;
